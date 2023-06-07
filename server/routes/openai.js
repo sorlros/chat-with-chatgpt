78357010099d353273bbe0e8619b37e231319a12
sorlros <sorlros2047@gmail.com>
2023-06-07 16:13:28 +0900

    completed

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -6,24 +6,21 @@ import { openai } from "../index.js";
 dotenv.config();
 const router = express.Router();
 
-router.post("/text", async (req, res) => {
+router.post("/code", async (req, res) => {
 	try {
 		const { text, activeChatId } = req.body;
-		console.log("🚀 ~ file: openai.js:12 ~ router.post ~ req.body:", req.body);
 
 		const response = await openai.createChatCompletion({
 			model: "gpt-3.5-turbo",
-			message: [
-				{ role: "system", content: "You are a helpful assistant." },
-				{ role: "user", content: text },
+			messages: [
+				{ role: "system", content: "You are a helpful assistant." }, // this represents the bot and what role they will assume
+				{ role: "user", content: text }, // the message that the user sends
 			],
 		});
 
-		console.log("data", response.data);
-
 		await axios.post(
-			`https://api.chatengine.io/chat${activeChatId}/messages/`,
-			{ text: response.data.choices[0].text },
+			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
+			{ text: response.data.choices[0].message.content },
 			{
 				headers: {
 					"Project-ID": process.env.PROJECT_ID,
@@ -33,9 +30,59 @@ router.post("/text", async (req, res) => {
 			}
 		);
 
-		res.status(200).json({ text: response.data.choices[0].text });
+		res.status(200).json({ text: response.data.choices[0].message.content });
 	} catch (error) {
-		console.error("error", error);
+		console.error("error", error.response.data.error);
+		res.status(500).json({ error: error.message });
+	}
+});
+
+router.post("/text", async (req, res) => {
+	try {
+		const { text, activeChatId } = req.body;
+
+		const response = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{ role: "system", content: "You are a helpful assistant." }, // this represents the bot and what role they will assume
+				{ role: "user", content: text }, // the message that the user sends
+			],
+		});
+
+		await axios.post(
+			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
+			{ text: response.data.choices[0].message.content },
+			{
+				headers: {
+					"Project-ID": process.env.PROJECT_ID,
+					"User-Name": process.env.BOT_USER_NAME,
+					"User-Secret": process.env.BOT_USER_SECRET,
+				},
+			}
+		);
+
+		res.status(200).json({ text: response.data.choices[0].message.content });
+	} catch (error) {
+		console.error("error", error.response.data.error);
+		res.status(500).json({ error: error.message });
+	}
+});
+
+router.post("/assist", async (req, res) => {
+	try {
+		const { text } = req.body;
+
+		const response = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{ role: "system", content: "You are a helpful assistant." }, // this represents the bot and what role they will assume
+				{ role: "user", content: `Finish my thought: ${text}` }, // the message that the user sends
+			],
+		});
+
+		res.status(200).json({ text: response.data.choices[0].message.content });
+	} catch (error) {
+		console.error("error", error.response.data.error);
 		res.status(500).json({ error: error.message });
 	}
 });
